bfb1d8342bba0dfa0cb69f8b350380fcf954f6cf
Md-Atikul-Islam-420 <mdatikulislam2314@gmail.com>
2025-04-06 19:00:13 +0600

    Incidents1 section is complete

--- src/components/all-pages/incidents/Incidents.jsx
+++ src/components/all-pages/incidents/Incidents.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
  
+ import Card from '../../common/card';
+ import IncidentsNavbar from './IncidentsNavbar';
  
  function Incidents() {
    return (
      <>
      
+     <IncidentsNavbar/>
+     <Card/>
      </>
    )
  }
- 
- export default Incidents;
